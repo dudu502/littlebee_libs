8fb59124d27cfa915aac7adee775f08efd8005b8
Haijie HJ2 Pan <panhj2@lenovo.com>
2024-07-31 13:41:03 +0800

    sample3 res

---
+++ Examples/Clients/LockstepClient/Assets/Main/Sample3/Scripts/Sample3.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample3 : Sample
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    protected override void Start()
+    {
+        base.Start();
+    }
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
